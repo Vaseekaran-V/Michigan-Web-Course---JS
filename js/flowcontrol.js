48185f9bfe2b171e768b37eccd816822146bd8a7
Vaseekaran <vvasee1996@gmail.com>
2020-06-26 00:21:45 +0530

    Added conditions to skip null values in array

--- js/flowcontrol.js
+++ js/flowcontrol.js
@@ -1,19 +1,26 @@
 var name = prompt("What is your name?")
 
 if(name.length != 0){
-    document.write("Hello " + name);
+    document.write("Hello " + name + "<br>");
 }else{
     document.write("Empty array <br>");
 }
 
 function getAverage(array){
     var sum = 0;
-    if(array.length != 0){
+    var counter = 0;
+    if(array.length != 0){ //to check if there are any elements in the array
         for(index = 0; index < array.length; index++){
+            if(array[index] == null){ //to skip when the element is null
+                counter++;
+                continue;
+            }
             sum += array[index];
         }
-        var average = parseFloat(sum / array.length);
-        document.write(average);
+        var lengthOfArray = array.length;
+        var totalNumbers = lengthOfArray - counter;
+        var average = parseFloat(sum / totalNumbers);
+        document.write("Average = " + average + "<br>");
     }else{
         document.write("empty array");
         document.write("<br>try again");
@@ -21,5 +28,5 @@ function getAverage(array){
     
 }
 
-var grades = [2, 5, 6, 8];
+var grades = [2, 5, , , 9, 8, , 8];
 getAverage(grades);
