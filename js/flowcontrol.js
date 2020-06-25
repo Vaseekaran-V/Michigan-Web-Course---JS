81e24f5a1f723c2bf0e5a942be1e559f0f5b7a1c
Vaseekaran <vvasee1996@gmail.com>
2020-06-25 21:05:26 +0530

    Added if-else condtion to coding

--- js/flowcontrol.js
+++ js/flowcontrol.js
@@ -2,4 +2,24 @@ var name = prompt("What is your name?")
 
 if(name.length != 0){
     document.write("Hello " + name);
+}else{
+    document.write("Empty array <br>");
 }
+
+function getAverage(array){
+    var sum = 0;
+    if(array.length != 0){
+        for(index = 0; index < array.length; index++){
+            sum += array[index];
+        }
+        var average = parseFloat(sum / array.length);
+        document.write(average);
+    }else{
+        document.write("empty array");
+        document.write("<br>try again");
+    }
+    
+}
+
+var grades = [2, 5, 6, 8];
+getAverage(grades);
